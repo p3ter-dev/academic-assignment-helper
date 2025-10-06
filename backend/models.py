6fc2b73660f6d3d2fa51ffcd14af3de97368daf2
peter <peterkinfe548@gmail.com>
2025-10-07 00:05:29 +0300

    model for database

--- backend/models.py
+++ backend/models.py
@@ -6,6 +6,13 @@ from sqlalchemy.dialects.postgresql import ARRAY
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
 class Student(Base):
     __tablename__ = "students"
     id = Column(Integer, primary_key=True)
